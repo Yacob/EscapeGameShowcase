b6169c0bdf8c7cc2150bd18a1d0b60d5b0190e04
yacob <nestor.jac@gmail.com>
2014-02-22 16:14:39 -0500

    Player drops breadcrumb

---
+++ Assets/_Scripts/CameraScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public Transform 	poi; // Point of Interest
+	public Vector3		offset = new Vector3(0,0,-10);
+	
+	void Update () {
+		Vector3 poiV3 = poi.position;
+		poiV3 += offset;
+		transform.position = poiV3;
+	}
+}
--- Assets/_Scripts/PlayerScript.cs
+++ Assets/_Scripts/PlayerScript.cs
@@ -5,12 +5,22 @@ public class PlayerScript : MonoBehaviour {
 
 	public float MaxSpeed = 4;
 	public Vector3 movement;
+	public Transform breadCrumb;
 
 	void Update () {
-		move();
+		Move();
+		DropBreadCrumb();
 	}
 	
-	void move(){
+	void DropBreadCrumb(){
+		Transform crumb;
+		crumb = (Transform)Instantiate(breadCrumb, transform.position, Quaternion.identity);
+		crumb.name = string.Format("({0}, {1})", transform.position.x, transform.position.y);
+		crumb.parent = transform;
+	}
+	
+	
+	void Move(){
 		movement = decelerate();
 		rigidbody.velocity = new Vector3(0, 0, 0);
 	
@@ -48,19 +58,19 @@ public class PlayerScript : MonoBehaviour {
 	Vector3 decelerate(){
 		Vector3 temp = rigidbody.velocity;
 		if(rigidbody.velocity.y > 0){
-			temp.y -= .01f;
+			temp.y -= .02f;
 			rigidbody.velocity = temp;			
 		}
 		else if(rigidbody.velocity.y < 0){
-			temp.y += .01f;
+			temp.y += .02f;
 			rigidbody.velocity = temp;	
 		}
 		
 		if(rigidbody.velocity.x > 0){
-			temp.x -= .01f;	
+			temp.x -= .02f;	
 		}
 		else if(rigidbody.velocity.x < 0){
-			temp.x += .01f;
+			temp.x += .02f;
 		}
 		return temp;
 	}
