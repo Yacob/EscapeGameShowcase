df0aee15be34ae975dd40802c66968c6abc56803
nickmagus <nickmagus@gmail.com>
2014-02-22 04:26:24 -0500

    added doge
    -follows breadcrumbs
    -randomly wanders

---
+++ Assets/BreadcrumbScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreadcrumbScript : MonoBehaviour {
+
+	public float lifetime = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifetime -= Time.deltaTime;
+		if (lifetime<=0){
+			Destroy(this.gameObject);
+		}
+	}
+}
---
+++ Assets/DogeController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DogeController : MonoBehaviour {
+	
+	public bool barking = false;
+	public float scent_range = 5;
+	public float speed = 1;
+	public bool has_crumb = false;
+	public GameObject crumb_to_follow;
+	
+	public Vector3 start_pos;
+	public Vector3 dest;
+	public float wander_radius = 5;
+
+	// Use this for initialization
+	void Start () {
+		start_pos = this.gameObject.transform.position;
+		GenerateDest ();
+	}
+	
+	void GenerateDest () {
+		Vector2 d = Random.insideUnitCircle*wander_radius;
+		dest = start_pos;
+		dest.x += d.x;
+		dest.y += d.y;
+	}
+
+	void CheckForCrumbs() {
+		GameObject [] crumbs = GameObject.FindGameObjectsWithTag ("breadcrumb");
+		float max_crumb_time = 0;
+		has_crumb = false;
+		foreach (GameObject crumb in crumbs) {
+			if (Vector3.Distance(crumb.transform.position, this.gameObject.transform.position)<scent_range){
+				float lt = crumb.GetComponent<BreadcrumbScript>().lifetime;
+				if ( lt > max_crumb_time || !has_crumb){
+					max_crumb_time = lt;
+					has_crumb = true;
+					crumb_to_follow = crumb;
+				}
+			}
+		}
+	}
+	
+	void MoveToDest(Vector3 dest, float t) {
+		Vector3 p = this.gameObject.transform.position;
+		if (Vector3.Distance(dest,p)<speed*t){
+			this.gameObject.transform.position = dest;
+		} else {
+			Vector3 d = Vector3.Normalize(dest - p);
+			this.gameObject.transform.position = p + d*speed*t;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CheckForCrumbs();
+		MoveToDest(has_crumb ? crumb_to_follow.transform.position:dest, Time.deltaTime);
+		if (Vector3.Distance(this.transform.position,dest)==0)
+			GenerateDest();
+	}
+}
