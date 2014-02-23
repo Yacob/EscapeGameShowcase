4dcf4ebb77991e128e63086525d1153faeca498d
Luke Anderson <lithas@umiche.du>
2014-02-22 19:26:42 -0500

    updates Hunters

--- Assets/_Scripts/HunterScript.cs
+++ Assets/_Scripts/HunterScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SlaveHunter : MonoBehaviour {
+public class HunterScript : MonoBehaviour {
 
 	public float viewAngle;
 	public float viewRadius;
@@ -12,20 +12,26 @@ public class SlaveHunter : MonoBehaviour {
 	public float patrolTime;
 	public float patrolDir;
 
+	public Material lineMaterial;
+
 	private float curTime;
 	private float curDir;
 	private float turnTime;
 	private bool isPatrol = true;
-	
+
+	private LineRenderer line;
+
 	private Vector3 homeLoc;
 
 	// Use this for initialization
 	void Start () {
+		line = this.gameObject.AddComponent<LineRenderer>();
 		StartPatrol();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		DrawCone();
 		GameObject player = GameObject.Find ("Player");
 		Vector3 PlayerPos = player.transform.position;
 		Vector3 curPos = transform.position;
@@ -60,7 +66,7 @@ public class SlaveHunter : MonoBehaviour {
 		if(!isPatrol){
 			StartPatrol();
 		}		
-		this.transform.position += this.transform.forward * moveSpd*Time.deltaTime;
+		//this.transform.position += this.transform.forward * moveSpd*Time.deltaTime;
 		curTime -= Time.deltaTime;
 		if(curTime <= 0){
 			curDir *= -1;
@@ -93,6 +99,17 @@ public class SlaveHunter : MonoBehaviour {
 		transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnTime);
 		turnTime += .01f;
 		this.transform.position += this.transform.forward * moveSpd*Time.deltaTime;
+	}
+
+	void DrawCone(){
+		float width = Mathf.Tan(1/2 * viewAngle*Mathf.PI/180) * viewRadius;
+		line.SetWidth(0, width);
+		line.SetVertexCount(2);
+		line.material = lineMaterial;
+		line.renderer.enabled = true;
+		line.SetPosition(0, transform.position);
+		line.SetPosition(1, transform.position + transform.forward * viewRadius);
+		Debug.Log("done drawing");
 	}
 
 }
